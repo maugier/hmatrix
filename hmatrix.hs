99c596917ddecc10e03c31c5cabaae62a5741483
Maxime Augier <max@xolus.net>
2013-07-25 12:28:15 +0200

    Final

--- hmatrix.hs
+++ hmatrix.hs
@@ -1,6 +1,9 @@
 import Control.Concurrent (threadDelay)
 import Control.Monad
 import Control.Monad.State
+import Data.Array.IO
+import Data.Char
+import Data.IORef
 import Data.Text (pack)
 import UI.NCurses
 import System.Random
@@ -10,55 +13,63 @@ import System.Random
 hira = ['ぁ'..'ゔ']
 kata = ['ァ'..'ヺ']
 
-message = kata ++ "THEGAME"
+message = map chr [84,72,69,71,65,77,69]
 
-delay = 500
+delay = 50000
 
-on = (4, 10)
-off = (2, 20)
+on = (2, 5)
+off = (10, 20)
 
 -- end config
 
-data Cell = CEmpty | CNormal | CBright
+data Cell = CEmpty | CNormal | CBright deriving Show
 
 slice n = map (take n) . takeWhile (not.null) . iterate (drop n)
 
 fi = fromInteger
 
-backdrop x y = take (fi y) . slice (fi x) . cycle
+backdrop y x = take (fi y) . slice (fi x) . cycle
 
-allblack x y = replicate (fi y) (replicate (fi x) CEmpty)
-
-createColors = sequence [ newColorID ColorRed ColorBlack 1 
+createColors = sequence [ newColorID ColorBlack ColorBlack 1 
                         , newColorID ColorGreen ColorBlack 2 ]
 
-crand :: (Integer,Integer) -> StateT a Curses Integer
-crand = lift . liftIO . randomRIO
+mutateArray f a = getBounds a >>= mapM_ (\i -> readArray a i >>= f >>= writeArray a i) . range
 
-setStyle [black,_] CEmpty  = setColor black
+updateCounter :: (Int, Cell) -> IO (Int, Cell)
+updateCounter (0, CEmpty) = return (0, CBright)
+updateCounter (0, CBright) = randomRIO on >>= \x -> return (x, CNormal)
+updateCounter (0, CNormal) = randomRIO off >>= \x -> return (x, CEmpty)
+updateCounter (n, s) = return (n-1, s)
+
+setStyle [black,_] CEmpty  = setColor black >> setAttribute AttributeBold False
 setStyle [_,green] CNormal = setColor green >> setAttribute AttributeBold False
 setStyle [_,green] CBright = setColor green >> setAttribute AttributeBold True
 
 
-drawSingleChar colors x y ch s = setStyle colors s >> moveCursor x y >> drawText (pack [ch])	
+drawSingleChar colors y x ch s = moveCursor y x >> setStyle colors s >> drawText (pack [ch])	
 
-deepzip f back mask = [ f x y c s | (x,(bl,ml)) <- zip [0..] (zip back mask)
-                                , (y,(c,s)) <- zip [0..] (zip bl ml)]
+deepzip f back mask = [ f y x c s | (y,(bl,ml)) <- zip [0..] (zip back mask)
+                                , (x,(c,s)) <- zip [0..] (zip bl ml)]
 
-redrawMatrix colors back mask = sequence_ $ deepzip (drawSingleChar colors) back mask
+roll m l = modifyIORef m ((l:) . init)
 
+redrawMatrix colors = (sequence_.) . deepzip (drawSingleChar colors)
 
-renderAndPause = render >> liftIO (threadDelay delay)
+initcounters :: Integer -> IO (IOArray Integer (Int,Cell))
+initcounters width = newArray (0, width - 1) (0, CNormal)
 
 main = runCurses $ do
-	--(x, y) <- screenSize
-	let (x,y) = (10,10)
+	(y, x') <- screenSize
+	let x = x' - 1 -- work around UI.Ncurses bug
 	colors <- createColors
 	win    <- defaultWindow
-	let back = backdrop x y message
-	let mask = backdrop x y [CEmpty, CNormal, CBright]
-	flip runStateT (allblack x y) . forever $ do
-		x <- crand (10,20)
-		mask <- get
-		lift . updateWindow win $ redrawMatrix colors back mask
-		lift renderAndPause
+	counters <- liftIO $ initcounters x
+	let back = backdrop y x message
+	mask <- liftIO $ newIORef (backdrop y x [CEmpty])
+	forever $ do
+		liftIO $ do 
+			mutateArray updateCounter counters
+			getElems counters >>= roll mask . map snd
+		liftIO (readIORef mask) >>= (updateWindow win . redrawMatrix colors back)
+		render
+		liftIO $ threadDelay delay
